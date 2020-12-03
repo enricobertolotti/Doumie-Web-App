60a98c5ee59dc089cb8bc9ac4f77f6ead6d82e4e
enricobertolotti <hello@enricobertolotti.com>
2020-12-03 22:11:12 +0100

    💄 Cosmetic Surgery

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
 
+import firebase from "firebase";
+
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+let app: unknown;
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log(user);
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
