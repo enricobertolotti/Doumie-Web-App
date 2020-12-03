1ecbc32565cb893f73b2d4d213aef80f875dcc85
enricobertolotti <hello@enricobertolotti.com>
2020-12-03 21:57:44 +0100

    Added firebase services and login / register components

---
+++ src/components/login/Register.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="wrapper">
+    <h1>Register component</h1>
+    <form @submit.prevent="registerUser">
+      <input
+        type="text"
+        placeholder="Firstname"
+        v-model="userData.firstname"
+      /><br />
+      <input
+        type="text"
+        placeholder="Lastname"
+        v-model="userData.lastname"
+      /><br />
+      <input
+        type="text"
+        placeholder="Username"
+        v-model="userData.username"
+      /><br />
+      <input type="email" placeholder="Email" v-model="userData.email" /><br />
+      <input
+        type="password"
+        placeholder="Choose a password"
+        v-model="userData.password"
+      /><br />
+      <button>Create account</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+// import db from "@/firestore";
+
+export default {
+  name: "Register",
+  data() {
+    return {
+      userData: {
+        firstname: "",
+        lastname: "",
+        username: "",
+        email: "",
+        password: ""
+      },
+      successMessage: "",
+      errorMessage: ""
+    };
+  },
+  methods: {
+    registerUser() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(
+          this.userData.email,
+          this.userData.password
+        )
+        .then(user => {
+          console.log(user);
+        })
+        .catch(err => {
+          this.errorMessage = err.message;
+        });
+    }
+  }
+};
+</script>
---
+++ src/firestore.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvoB52SNCXbGjv8pzENeohURidOhYudlE",
+  authDomain: "dome-cd504.firebaseapp.com",
+  databaseURL: "https://dome-cd504.firebaseio.com",
+  projectId: "dome-cd504",
+  storageBucket: "dome-cd504.appspot.com",
+  messagingSenderId: "416952278030",
+  appId: "1:416952278030:web:a3792290358ffd9b1db05f"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+export default db;
