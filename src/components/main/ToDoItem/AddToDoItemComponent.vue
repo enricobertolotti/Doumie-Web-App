b20883bf13db84ba83bf80ad13057e4a7ffd58df
enricobertolotti <hello@enricobertolotti.com>
2020-12-12 17:58:57 +0100

    updated gitignore

---
+++ src/components/main/ToDoItem/AddToDoItemComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="d-flex w-100">
+
+  </div>
+</template>
+
+<script>
+import { Priority } from '@/models/ToDoItem';
+  export default {
+    data() {
+      return {
+        toDoItem: {
+          title: "Title",
+          description: "Decription",
+          dueDate: new Date(),
+          assignee: this.currentUser,
+          priority: Priority.low,
+        }
+      }
+    },
+    computed: {
+      currentUser() {
+        return this.$store.getters.currentUser();
+      }
+    },
+    methods: {
+      submitTodoItem() {
+        this.$store.dispatch("addTodoItem", this.toDoItem)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
